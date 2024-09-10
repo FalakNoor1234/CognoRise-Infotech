b4409b247256d15262d20df3aca80420fe9cc9d3
RAFIQUE12456 <murk88888@gmail.com>
2024-09-10 10:23:12 +0500

    Update list.js

--- CognoRise Infotech/todo list task/list.js
+++ CognoRise Infotech/todo list task/list.js
@@ -1,39 +1,3 @@
-// const item = document.querySelector("#items");
-// const toDoBox = document.querySelector("#to-do-box");
-
-// item.addEventListener("keyup", function(event) {  /*addevent always occur in parenthesis() not curly braces{}*/
-  
-//     if (event.key === "Enter") {
-//         addToDo(this.value);
-//         this.value = "";
-//     }
-// });
-
-// const addToDo = (item) => {
-//     const listItem = document.createElement("li");
-    
-//     // Corrected the template literal syntax
-//     listItem.innerHTML = `${item} <i class="fas fa-times"></i>`;
-    
-//     // Corrected the addEventListener syntax
-//     listItem.addEventListener("click", function() {
-//         this.classList.toggle("done");
-//     });
-//      listItem.querySelector("i").addEventListener(
-//         "click",
-//         function(){
-//             listItem.remove()
-//         }
-//      )
-//     toDoBox.appendChild(listItem);
-// };
-
-
-
-
-
-
-
 const itemInput = document.querySelector("#items");
 const addButton = document.querySelector("#add-button");
 const toDoBody = document.querySelector("#to-do-body");
